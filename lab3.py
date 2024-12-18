a0f9e4da76f91270cabe7bb03ecba77b17bb9876
RiBA-XI <71338296+RiBA-XI@users.noreply.github.com>
2024-12-18 11:03:05 +0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,25 +1,28 @@
 from math import *
 
 def task1(x):
-    x = int(x)
-    def prime_number(x):
-        for i in range(2, (x/2)+1):
-            if x%i==0:
-                return print("число x не является простым")
-        else:
-            return print("число x простое")
-    def positive_number(x):
-        if x>0:
-            return print("число x положительное")
-        elif x==0:
-            return print("число x равно 0")
-        else:
-            return print("число x не является положительным")
+    while True:
+        x = int(x)
+        def prime_number(x):
+            for i in range(2, (x/2)+1):
+                if x%i==0:
+                    return print("число x не является простым")
+            else:
+                return print("число x простое")
+        def positive_number(x):
+            if x>0:
+                return print("число x положительное")
+            elif x==0:
+                return print("число x равно 0")
+            else:
+                return print("число x не является положительным")
+        prime_number(int(x))
+        positive_number(x)
 
 def task2(x):
      x = int(x)
      if x >=0:
-         return e**x+x
+         return exp(x)+x
      else:
          return sin(x)+1
 
@@ -45,5 +48,18 @@ def task5(a, b, c):
     l.append(b)
     l.append(c)
     print("наименьшее значение", l.min())
+    print("наибольшее значение", l.min())
 
-task5(input(), input(), input())
+def task6(number):
+    def is_happy(number):
+        if number[0]+number[1]+number[2]==number[3]+number[4]+number[5]:
+            return True
+        return False
+
+def task8(x):
+    xps=2
+    s = exp(-x)-exp(-2*x)
+    while xps<x:
+        xps += 1
+        s += exp(-1*xps)
+    return s
