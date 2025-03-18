31204d2f129d31b096e370d044895f9060b3e28d
RiBA-XI <71338296+RiBA-XI@users.noreply.github.com>
2025-03-18 19:17:31 +0300

    Update pr2.py

--- pr2.py
+++ pr2.py
@@ -33,7 +33,12 @@ class MainWindow(QMainWindow):
             range_start = 0
             range_end = 1
 
-        x = np.linspace(range_start, range_end, int(self.point_count_input.text()))
+        try:
+            num_point = int(self.num_points_input.text())
+        except ValueError:
+            num_point = 50
+
+        x = np.linspace(range_start, range_end, num_point)
         functions = {}  # определим словарь функций
         exec(f"def f(x): return {self.selected_function}", functions)
         function = functions["f"]
@@ -47,15 +52,16 @@ class MainWindow(QMainWindow):
         plt.title('График функции: ' + self.selected_function)
         self.canvas.draw()
 
+        self.x = x
+        self.y = y
+
     def clear_plot(self):
         try:
             self.figure.clf()  # Очищаем всю figure
             self.canvas.draw()
         except Exception as e:
             QMessageBox.critical(self, "Ошибка", f"Ошибка при очистке графика: {e}")
-    def save_plot_data(x, y): #save_points_to_txt
-        with open("hello.txt", "w") as my_file:
-            my_file.write("")
+
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -79,8 +85,10 @@ class MainWindow(QMainWindow):
         self.range_label = QLabel("Диапазон по оси x:")
         self.range_start_input = QLineEdit('0')
         self.range_end_input = QLineEdit('1')
-        self.num_points = QLabel("количество точек для построения графика:")
-        self.point_count_input = QLineEdit('50')
+        self.plot_point_count_label = QLabel("Количество точек для отрисовки графика")
+        self.num_points_input = QLineEdit('50')
+        self.save_plot_points_button = QPushButton("сохранить точки графика в файл")
+        self.save_plot_points_button.clicked.connect(self.save_plot_points)
 
         # Создание центрального виджета
         central_widget = QWidget()
@@ -98,8 +106,9 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.range_label)
         layout.addWidget(self.range_start_input)
         layout.addWidget(self.range_end_input)
-        layout.addWidget(self.num_points)
-        layout.addWidget(self.point_count_input)
+        layout.addWidget(self.plot_point_count_label)
+        layout.addWidget(self.num_points_input)
+        layout.addWidget(self.save_plot_points_button)
 
         # Установка центрального виджета
         self.setCentralWidget(central_widget)
@@ -129,6 +138,14 @@ class MainWindow(QMainWindow):
             self.selected_function = selected_items[0].text()
         else:
             self.selected_function = None
+
+    def save_plot_points(self):
+        filename = "plot_data.txt"
+        with open(filename, "w") as f:
+            for i in range(len(self.x)):
+                f.write(f"{self.x[i]}\t{self.y[i]}\n")
+            QMessageBox.information(self, "Успешно", f"Данные графика успешно сохранены в файл {filename}")
+
 
 # Создать приложение QApplication
 app = QApplication([])
